1c6b5194597035a5ec3dfcb681b2b63c725cddc2
nscodechris <christoffer.vdmab@gmail.com>
2022-10-19 13:51:48 +0200

    new

---
+++ hm.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+url = "https://www.github.com"
+
+# https://www.nordea.se/privat/produkter/aktuella-priser-och-rantor.html
+proxy = "http://serviceproxy.se.shb.biz:8130"
+
+page = requests.get(url,
+                    proxies={"http": proxy, "https": proxy}, headers=headers)
+
+# print(page.text)
+soup = BeautifulSoup(page.text, 'html.parser')
+fake_dict = {"item": [1], "picture": []}
+list_use = []
+i = 0
+# print(soup)
+for item in soup.find_all('img'):
+    if item['src'].endswith('png'):
+        fake_dict["picture"] = item['src']
+        list_use.append(fake_dict.copy())
+
+
+table = pd.DataFrame(list_use)
+
+def render_imgs(path: str = None) -> str:
+    return f"""<img src="{path}" width="60" > """
+
+table.to_html("table.html", escape=False, formatters=dict(picture=render_imgs))
